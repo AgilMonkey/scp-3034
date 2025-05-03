9ae5bba3c3701f6fd988220b757d6816851b2bd2
Aepu <muhammadagilfadilah@gmail.com>
2025-05-04 02:52:49 +0800

    Added interview and debrief paper

---
+++ entities/papers/common/paper.gd
@@ -0,0 +1,35 @@
+class_name Paper
+extends Node3D
+
+
+@export var paper_ui: CanvasLayer
+@export var exit_button: Button
+@export var interact: Interactable3D
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		_on_exit_button_button_down()
+
+
+func _ready() -> void:
+	interact.interacted.connect(_on_interact_area_interacted)
+	exit_button.button_down.connect(_on_exit_button_button_down)
+	paper_ui = paper_ui
+	paper_ui.hide()
+	set_process_input(false)
+
+
+func _on_interact_area_interacted() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	set_process_input(true)
+	var player = get_tree().get_first_node_in_group("player")
+	player.process_mode = Node.PROCESS_MODE_DISABLED
+	paper_ui.show()
+
+
+func _on_exit_button_button_down() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	var player = get_tree().get_first_node_in_group("player")
+	player.process_mode = Node.PROCESS_MODE_INHERIT
+	paper_ui.hide()
