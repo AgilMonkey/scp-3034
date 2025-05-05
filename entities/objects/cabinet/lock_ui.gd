ffff8b105ad9aee0e0ad9202a8673c0ceb61974c
Aepu <muhammadagilfadilah@gmail.com>
2025-05-05 10:25:33 +0800

    Simple password system' '

--- entities/objects/cabinet/lock_ui.gd
+++ entities/objects/cabinet/lock_ui.gd
@@ -1,12 +1,17 @@
 extends Control
 
 
+var password = "1234"
+
 @export var lock_buttons: Array[Control]
 
 
 func _on_enter_button_button_down() -> void:
 	var lock_pass = get_lock_pass()
-	print(lock_pass)
+	if lock_pass == password:
+		print("Correct!")
+	else:
+		print("NO!")
 
 
 func get_lock_pass() -> String:
