57fcbec4ac0e20c5cfb0c91b10911ab831a1d00e
Aepu <muhammadagilfadilah@gmail.com>
2025-05-11 22:52:57 +0800

    Added printer light and sound

--- entities/objects/printer/printer.gd
+++ entities/objects/printer/printer.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 @onready var last_hint: Node3D = $LastHint
 @onready var attention_light: MeshInstance3D = $AttentionLight
+@onready var attention_audio: AudioStreamPlayer3D = $AttentionAudio
 
 var light_on := false
 
@@ -12,6 +13,12 @@ func _start_printing_last_hint():
 	last_hint.process_mode = Node.PROCESS_MODE_INHERIT
 	
 	start_attention_light()
+	attention_audio.play()
+
+
+func stop_attention():
+	light_on = false
+	attention_audio.stop()
 
 
 func start_attention_light():
@@ -25,8 +32,12 @@ func start_attention_light():
 		attention_light.set_surface_override_material(0, light_mat)
 		await get_tree().create_timer(0.5).timeout
 		if not light_on:
+			light_mat.emission = Color.BLACK
+			attention_light.set_surface_override_material(0, light_mat)
 			break
 
 
 func _on_interactable_3d_interacted() -> void:
 	DialogueManager.start_dialogue("You've done well. Finish the job", 3.0)
+	await get_tree().create_timer(3.0).timeout
+	stop_attention()
